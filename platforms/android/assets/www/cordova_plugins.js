5fe3dffce93c4196c2b50e3b0842d99f97894e26
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-07-26 02:29:13 +0300

    Build android

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -12,8 +12,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.2",
-    "cordova-plugin-keyboard": "1.1.5"
+    "cordova-plugin-keyboard": "1.1.5",
+    "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
 });
